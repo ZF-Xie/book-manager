c607f32d596635869529a9824beb1c7c92b0417c
樊 <zhouziyanya@qq.com>
2025-06-28 18:25:55 +0800

    giao

--- bookmgr/cell/cell_main.cpp
+++ bookmgr/cell/cell_main.cpp
@@ -4,11 +4,29 @@
 Cell_Main::Cell_Main(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::Cell_Main)
+    ,m_userPage(nullptr)
+    ,m_bookPage(nullptr)
+    ,m_recordPage(nullptr)
+    ,m_replyPage(nullptr)
 {
     ui->setupUi(this);
+    initPage();
 }
 
 Cell_Main::~Cell_Main()
 {
     delete ui;
+}
+
+void Cell_Main::initPage()
+{
+    m_userPage=new Cell_UserMgr(this);
+    m_bookPage=new Cell_Bookmgr(this);
+    m_recordPage=new Cell_Record(this);
+    m_replyPage=new Cell_Reply(this);
+    ui->stackedWidget->addWidget(m_userPage);
+    ui->stackedWidget->addWidget(m_bookPage);
+    ui->stackedWidget->addWidget(m_recordPage);
+    ui->stackedWidget->addWidget(m_replyPage);
+    ui->stackedWidget->setCurrentIndex(0);
 }
--- bookmgr/cell/cell_main.h
+++ bookmgr/cell/cell_main.h
@@ -2,7 +2,10 @@
 #define CELL_MAIN_H
 
 #include <QMainWindow>
-
+#include "cell_usermgr.h"
+#include "cell_bookmgr.h"
+#include "cell_record.h"
+#include "cell_reply.h"
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class Cell_Main;
@@ -16,8 +19,13 @@ class Cell_Main : public QMainWindow
 public:
     Cell_Main(QWidget *parent = nullptr);
     ~Cell_Main();
+    void initPage();
 
 private:
     Ui::Cell_Main *ui;
+    Cell_UserMgr*m_userPage;
+    Cell_Bookmgr*m_bookPage;
+    Cell_Record*m_recordPage;
+    Cell_Reply*m_replyPage;
 };
 #endif // CELL_MAIN_H
