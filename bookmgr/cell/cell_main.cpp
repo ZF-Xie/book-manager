41f007121be7d52a1643fe19eee94cc895ee68a6
樊 <zhouziyanya@qq.com>
2025-06-28 23:11:15 +0800

    giao

--- bookmgr/cell/cell_main.cpp
+++ bookmgr/cell/cell_main.cpp
@@ -1,6 +1,7 @@
 #include "cell_main.h"
 #include "ui_cell_main.h"
-
+#include<QPushButton>
+#include<QtDebug>
 Cell_Main::Cell_Main(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::Cell_Main)
@@ -29,4 +30,51 @@ void Cell_Main::initPage()
     ui->stackedWidget->addWidget(m_recordPage);
     ui->stackedWidget->addWidget(m_replyPage);
     ui->stackedWidget->setCurrentIndex(0);
+    
+    auto l = ui->tool->children();
+    for (auto it : l)
+    {
+        if (it->objectName().contains("btn"))
+        {
+            connect(static_cast<QPushButton*>(it),&QPushButton::clicked,this,&Cell_Main::dealMenu);
+        }
+    }
+}
+
+void Cell_Main::dealMenu()
+{
+    auto str = qDebug() << sender()->objectName();
+
+    do
+    {
+        if ("btn_user"==str)
+        {
+            ui->stackedWidget->setCurrentIndex(0);
+            break:
+        }
+
+        if ("btn_book" == str)
+        {
+            ui->stackedWidget->setCurrentIndex(1);
+            break:
+        }
+
+        if ("btn_his" == str)
+        {
+            ui->stackedWidget->setCurrentIndex(2);
+            break:
+        }
+
+        if ("toolButton" == str)
+        {
+            ui->stackedWidget->setCurrentIndex(3);
+            break:
+        }
+
+        if ("toolButton_2" == str)
+        {
+            ui->stackedWidget->setCurrentIndex(4);
+            break:
+        }
+    }while(false)
 }
