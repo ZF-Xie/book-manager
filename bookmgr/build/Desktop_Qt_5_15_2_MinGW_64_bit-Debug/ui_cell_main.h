b447124c6aa517f2ecb68468694762d156059583
樊 <zhouziyanya@qq.com>
2025-07-02 18:18:31 +0800

    oaig

--- bookmgr/build/Desktop_Qt_5_15_2_MinGW_64_bit-Debug/ui_cell_main.h
+++ bookmgr/build/Desktop_Qt_5_15_2_MinGW_64_bit-Debug/ui_cell_main.h
@@ -54,7 +54,7 @@ public:
         gridLayout_2->setObjectName(QString::fromUtf8("gridLayout_2"));
         tool = new QWidget(centralwidget);
         tool->setObjectName(QString::fromUtf8("tool"));
-        tool->setMaximumSize(QSize(120, 16777215));
+        tool->setMaximumSize(QSize(160, 16777215));
         gridLayout = new QGridLayout(tool);
         gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
         toolButton_2 = new QToolButton(tool);
